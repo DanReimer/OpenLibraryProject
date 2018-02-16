6ac5503bc25b9cbccf322156bce050f8de471c27
DanReimer <dajudoguy@gmail.com>
2018-02-16 18:07:34 +0000

    Added home and about controllers. Started home view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
+  get 'home/index'
+
   resources :covers
   resources :identifiers
   resources :authors
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
